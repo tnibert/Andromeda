0d1d473a3105089f2e65a08538577b17b34b1756
Timothy Nibert <docz2a@gmail.com>
2026-05-03 21:58:10 +1000

    added a third level

--- levels/levelconfigs.py
+++ levels/levelconfigs.py
@@ -1,10 +1,9 @@
 import random
 
 from constants import SCROLLSPEED, TURRET_DIMENSION, SCREENW
-from loadstaticres import turretimg, gunimg
+from loadstaticres import turretimg, gunimg, invader_boss_image, surf_alien, lantern_saucer_img, magykal_boss_img, \
+    simple_saucer_img, earth_alien_boss_img, magykal_map, simple_map
 from sprites import boss
-import pygame
-
 from sprites.enemy import Enemy
 from sprites.turret import Turret
 from statemachines.cutcrossenemy import cut_cross_enemy_state_graph
@@ -12,37 +11,45 @@ from statemachines.defaultenemy import default_enemy_state_graph
 
 
 def l1_enemy_config(ship):
-    saucer_img = pygame.image.load("assets/magykal_level/saucer.png")
-    boss_image = pygame.image.load("assets/magykal_level/boss.png")
-
     # todo: enemy factory?
     entry_config = [
-        (SCROLLSPEED * 1, Enemy(saucer_img, random.randrange(0, SCREENW), default_enemy_state_graph)),
-        (SCROLLSPEED * 5, Enemy(saucer_img, random.randrange(0, SCREENW), default_enemy_state_graph)),
-        (SCROLLSPEED * 10, Enemy(saucer_img, SCREENW - 80, cut_cross_enemy_state_graph)),
-        (SCROLLSPEED * 15, Enemy(saucer_img, random.randrange(0, SCREENW), default_enemy_state_graph)),
-        (SCROLLSPEED * 15, Enemy(saucer_img, SCREENW - 80, cut_cross_enemy_state_graph)),
+        (SCROLLSPEED * 1, Enemy(surf_alien, random.randrange(0, SCREENW), default_enemy_state_graph)),
+        (SCROLLSPEED * 5, Enemy(surf_alien, random.randrange(0, SCREENW), default_enemy_state_graph)),
+        (SCROLLSPEED * 10, Enemy(surf_alien, SCREENW - 80, cut_cross_enemy_state_graph)),
+        (SCROLLSPEED * 15, Enemy(lantern_saucer_img, random.randrange(0, SCREENW), default_enemy_state_graph)),
+        (SCROLLSPEED * 15, Enemy(lantern_saucer_img, SCREENW - 80, cut_cross_enemy_state_graph)),
         (SCROLLSPEED * 15,
          Turret(40, -TURRET_DIMENSION, turretimg, gunimg, ship)),
         (SCROLLSPEED * 15,
          Turret(SCREENW - TURRET_DIMENSION - 40, -TURRET_DIMENSION, turretimg, gunimg, ship)),
-        (SCROLLSPEED * 25, boss.MagykalBossBehave(SCREENW / 2 - boss_image.get_width() / 2, -1200, boss_image, ship))
+        (SCROLLSPEED * 25,
+         boss.MagykalBossBehave(SCREENW / 2 - magykal_boss_img.get_width() / 2, -1200, magykal_boss_img, ship))
     ]
     entry_config.sort(key=lambda tup: tup[0])  # ensure ordered from earliest to latest
     return entry_config
 
 def l2_enemy_config(ship):
-    saucer_img = pygame.image.load("assets/simple_level/saucer.png")
-    saucer_img2 = pygame.image.load("assets/magykal_level/saucer.png")
-    boss_image = pygame.image.load("assets/simple_level/invader.png")
-
     entry_config = [
-        (SCROLLSPEED * 10, Enemy(saucer_img, random.randrange(0, SCREENW), default_enemy_state_graph)),
-        (SCROLLSPEED * 15, Enemy(saucer_img, random.randrange(0, SCREENW), default_enemy_state_graph)),
-        (SCROLLSPEED * 15, Enemy(saucer_img2, SCREENW - 80, cut_cross_enemy_state_graph)),
+        (SCROLLSPEED * 10, Enemy(simple_saucer_img, random.randrange(0, SCREENW), default_enemy_state_graph)),
+        (SCROLLSPEED * 15, Enemy(simple_saucer_img, random.randrange(0, SCREENW), default_enemy_state_graph)),
+        (SCROLLSPEED * 15, Enemy(lantern_saucer_img, SCREENW - 80, cut_cross_enemy_state_graph)),
         (SCROLLSPEED * 15,
          Turret(SCREENW/2 - TURRET_DIMENSION/2, -TURRET_DIMENSION, turretimg, gunimg, ship)),
-        (SCROLLSPEED * 25, boss.InvaderBossBehave(SCREENW / 2 - boss_image.get_width() / 2, -1200, boss_image, ship))
+        (SCROLLSPEED * 25,
+         boss.InvaderBossBehave(SCREENW / 2 - invader_boss_image.get_width() / 2, -1200, invader_boss_image, ship))
+    ]
+    entry_config.sort(key=lambda tup: tup[0])  # ensure ordered from earliest to latest
+    return entry_config
+
+def l3_enemy_config(ship):
+    entry_config = [
+        (SCROLLSPEED * 10, Enemy(surf_alien, random.randrange(0, SCREENW), default_enemy_state_graph)),
+        (SCROLLSPEED * 15, Enemy(surf_alien, random.randrange(0, SCREENW), default_enemy_state_graph)),
+        (SCROLLSPEED * 15, Enemy(lantern_saucer_img, SCREENW - 80, cut_cross_enemy_state_graph)),
+        (SCROLLSPEED * 15,
+         Turret(SCREENW / 2 - TURRET_DIMENSION / 2, -TURRET_DIMENSION, turretimg, gunimg, ship)),
+        (SCROLLSPEED * 25,
+         boss.MagykalBossBehave(SCREENW / 2 - earth_alien_boss_img.get_width() / 2, -1200, earth_alien_boss_img, ship))
     ]
     entry_config.sort(key=lambda tup: tup[0])  # ensure ordered from earliest to latest
     return entry_config
@@ -50,15 +57,21 @@ def l2_enemy_config(ship):
 # configurations for each level, in order
 level_configs = [
     {
-        "background": pygame.image.load("assets/magykal_level/map.png"),
+        "background": magykal_map,
         "bg_music_fname": "assets/JaricoIsland.ogg",
         "start_text": "LEVEL 1",
         "enemy_config": l1_enemy_config
     },
     {
-        "background": pygame.image.load("assets/simple_level/map.png"),
+        "background": simple_map,
         "bg_music_fname": "assets/JaricoLandscape.ogg",
         "start_text": "LEVEL 2",
         "enemy_config": l2_enemy_config
+    },
+    {
+        "background": magykal_map,
+        "bg_music_fname": "assets/JaricoLandscape.ogg",
+        "start_text": "LEVEL 3",
+        "enemy_config": l3_enemy_config
     }
 ]
--- loadstaticres.py
+++ loadstaticres.py
@@ -2,6 +2,9 @@ from core.utilfuncs import toframes
 from constants import TURRET_DIMENSION
 import pygame
 
+"""
+This file loads the static resources so that other modules can import them.
+"""
 
 introscreen = pygame.image.load("assets/intro1.png")
 
@@ -20,3 +23,15 @@ blank = pygame.Surface((1, 1))
 
 turretimg = pygame.transform.scale(pygame.image.load("assets/td_basic_towers/PNG/Tower.png"), (TURRET_DIMENSION, TURRET_DIMENSION))
 gunimg = pygame.transform.scale(pygame.image.load("assets/td_basic_towers/PNG/Cannon.png"), (TURRET_DIMENSION/1.5, TURRET_DIMENSION/1.5))
+
+# enemies
+surf_alien = pygame.transform.scale(pygame.image.load("assets/w/surf_alien.png"), (TURRET_DIMENSION, TURRET_DIMENSION))
+earth_alien_boss_img = pygame.transform.scale(pygame.image.load("assets/w/earth_alien.png"), (TURRET_DIMENSION*5, TURRET_DIMENSION*5))
+lantern_saucer_img = pygame.image.load("assets/magykal_level/saucer.png")
+magykal_boss_img = pygame.image.load("assets/magykal_level/boss.png")
+simple_saucer_img = pygame.image.load("assets/simple_level/saucer.png")
+invader_boss_image = pygame.image.load("assets/simple_level/invader.png")
+
+# maps
+magykal_map = pygame.image.load("assets/magykal_level/map.png")
+simple_map = pygame.image.load("assets/simple_level/map.png")
